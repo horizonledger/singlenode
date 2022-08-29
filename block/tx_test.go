5fd8fa459e60f0950edf1cb77bf5c47a5c9a1d20
benjyz <benjyc@proton.me>
2022-08-29 21:41:00 +0700

    tx test

--- block/tx_test.go
+++ block/tx_test.go
@@ -1,4 +1,9 @@
-package main
+package block
+
+import (
+	"encoding/json"
+	"testing"
+)
 
 // import (
 // 	"encoding/hex"
@@ -11,66 +16,66 @@ package main
 // 	"singula.finance/netio"
 // )
 
-// //basic block functions
-// func TestBasicAssign(t *testing.T) {
-// 	var tx Tx
-// 	tx = Tx{Nonce: 1}
-// 	if tx.Nonce != 1 {
-// 		t.Error("fail assign nonce")
-// 	}
-// }
+//basic block functions
+func TestBasicAssign(t *testing.T) {
+	var tx Tx
+	tx = Tx{Nonce: 1}
+	if tx.Nonce != 1 {
+		t.Error("fail assign nonce")
+	}
+}
 
-// func TestTxJson(t *testing.T) {
-// 	var tx Tx
-// 	tx = Tx{Nonce: 1}
-// 	txJson, _ := json.Marshal(tx)
-// 	if txJson[0] != '{' {
-// 		t.Error("start json")
-// 	}
-// 	i := len(txJson) - 1
-// 	if txJson[i] != '}' {
-// 		t.Error("end json")
-// 	}
+func TestTxJson(t *testing.T) {
+	var tx Tx
+	tx = Tx{Nonce: 1}
+	txJson, _ := json.Marshal(tx)
+	if txJson[0] != '{' {
+		t.Error("start json")
+	}
+	i := len(txJson) - 1
+	if txJson[i] != '}' {
+		t.Error("end json")
+	}
 
-// 	var newtx Tx
-// 	if err := json.Unmarshal(txJson, &newtx); err != nil {
-// 		panic(err)
-// 	}
-// 	if newtx.Nonce != tx.Nonce {
-// 		t.Error("json marshal failed")
-// 	}
-// 	if newtx.Sender != tx.Sender {
-// 		t.Error("json marshal failed")
-// 	}
-// }
+	var newtx Tx
+	if err := json.Unmarshal(txJson, &newtx); err != nil {
+		panic(err)
+	}
+	if newtx.Nonce != tx.Nonce {
+		t.Error("json marshal failed")
+	}
+	if newtx.Sender != tx.Sender {
+		t.Error("json marshal failed")
+	}
+}
 
-// func TestSignTxBasic(t *testing.T) {
+func TestSignTxBasic(t *testing.T) {
 
-// 	keypair := crypto.PairFromSecret("test")
-// 	pub := crypto.PubKeyToHex(keypair.PubKey)
-// 	//account := Account{AccountKey: crypto.Address(pub)}
+	// keypair := crypto.PairFromSecret("test")
+	// pub := crypto.PubKeyToHex(keypair.PubKey)
+	// //account := Account{AccountKey: crypto.Address(pub)}
 
-// 	randNonce := 0
-// 	amount := 10
+	// randNonce := 0
+	// amount := 10
 
-// 	genkeypair := chain.GenesisKeys()
-// 	addr := crypto.Address(crypto.PubKeyToHex(genkeypair.PubKey))
-// 	//Genesis_Account := AccountFromString(addr)
+	// genkeypair := chain.GenesisKeys()
+	// addr := crypto.Address(crypto.PubKeyToHex(genkeypair.PubKey))
+	// //Genesis_Account := AccountFromString(addr)
 
-// 	//{"Nonce":0,"Amount":0,"Sender":{"AccountKey":"Pa033f6528cc1"},"Receiver":{"AccountKey":"Pa033f6528cc1"},"SenderPubkey":"","Signature":"","id":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
-// 	r := crypto.Address(pub)
-// 	tx := Tx{Nonce: randNonce, Amount: amount, Sender: addr, Receiver: r, SenderPubkey: "", Signature: ""}
+	// //{"Nonce":0,"Amount":0,"Sender":{"AccountKey":"Pa033f6528cc1"},"Receiver":{"AccountKey":"Pa033f6528cc1"},"SenderPubkey":"","Signature":"","id":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+	// r := crypto.Address(pub)
+	// tx := Tx{Nonce: randNonce, Amount: amount, Sender: addr, Receiver: r, SenderPubkey: "", Signature: ""}
 
-// 	tx = crypto.SignTxAdd(tx, keypair)
+	// tx = crypto.SignTxAdd(tx, keypair)
 
-// 	//log.Println(tx)
+	// //log.Println(tx)
 
-// 	verified := crypto.VerifyTxSig(tx)
+	// verified := crypto.VerifyTxSig(tx)
 
-// 	if !verified {
-// 		t.Error("verify tx fail")
-// 	}
-// }
+	// if !verified {
+	// 	t.Error("verify tx fail")
+	// }
+}
 
 // func TestTxFile(t *testing.T) {
 // 	//write tx.json
