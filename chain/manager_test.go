e2f0ce9f3074e2f3878ed972a30910f11d7bdb21
benjyz <benjyc@proton.me>
2022-08-29 21:28:27 +0700

    test

--- chain/manager_test.go
+++ chain/manager_test.go
@@ -2,17 +2,27 @@ package chain
 
 import (
 	"testing"
+
+	"golang.org/x/exp/maps"
 )
 
 func TestBalance(t *testing.T) {
 
 	mgr := CreateManager()
-	//node.Mgr = &mgr
 	mgr.InitAccounts()
-	//initSyncChain(node.Config)
 
-	//genBlock := chain.MakeGenesisBlock()
-	//mgr.ApplyBlock(genBlock)
+	ks := maps.Keys(mgr.State.Accounts)
+
+	if mgr.State.Accounts[ks[0]] == 0 {
+		t.Error("0 balance")
+	}
+
+	genBlock := MakeGenesisBlock()
+	if genBlock.Height != 0 {
+		t.Error("wrong height")
+	}
+
+	mgr.ApplyBlock(genBlock)
 
 	// if reply.MessageType != netio.REP {
 	// 	t.Error("balance msg", reply_msg)
