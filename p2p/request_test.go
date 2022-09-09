be6b7be9ecf17f5901bf4528364e452236a535af
benjyz <benjyc@proton.me>
2022-09-09 13:48:39 +0700

    tx test

--- p2p/request_test.go
+++ p2p/request_test.go
@@ -1,10 +1,14 @@
 package p2p
 
 import (
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"singula.finance/netio"
+	"singula.finance/netio/crypto"
+	"singula.finance/node/block"
 	"singula.finance/node/chain"
 )
 
@@ -93,5 +97,39 @@ func TestBalanceReq(t *testing.T) {
 	if reply != string(jm) {
 		t.Error("process reply error ", reply, raw2)
 	}
+
+}
+func TestTxReq(t *testing.T) {
+
+	ntchan := netio.ConnNtchanStub("test", "testout")
+	netio.NetConnectorSetupMock(ntchan)
+
+	mgr := chain.CreateManager()
+	mgr.InitAccounts()
+
+	keypair_sender := crypto.PairFromSecret("sender")
+	pubkey_sender := crypto.PubKeyToHex(keypair_sender.PubKey)
+	addr_sender := crypto.Address(pubkey_sender)
+
+	keypair_receiver := crypto.PairFromSecret("receiver")
+	addr_receiver := crypto.Address(crypto.PubKeyToHex(keypair_receiver.PubKey))
+
+	// create tx
+	tx := block.Tx{Nonce: 1, Amount: 10, Sender: addr_sender, Receiver: addr_receiver, SenderPubkey: pubkey_sender}
+	// sign tx
+	signature := block.SignTx(tx, keypair_sender.PrivKey)
+	tx.Signature = hex.EncodeToString(signature.Serialize())
+
+	bb, _ := json.Marshal(tx)
+	raw := json.RawMessage(bb)
+	fmt.Println(raw)
+
+	if len(raw) == 0 {
+		t.Error("raw ", raw)
+	}
+
+	//TODO parse back and check
+
+	//TODO let mgr apply the tx
 
 }
